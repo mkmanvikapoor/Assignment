6a694054e7d9b139c20a551e4f95b52e577cc069
Manvi Kapoor <manvi.kapoor@shorthillstech.com>
2022-07-22 16:30:59 +0530

    Linux added

--- unix.sh
+++ unix.sh
@@ -1,1 +1,9 @@
-find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -newermt "2021-04-01" ! -newermt "2021-04-30"
+--To find the image files for the month of April--
+find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -ls -newermt "2021-04-01" ! -newermt "2021-04-30"
+--To find the number of images of April month--
+find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -newermt "2022-04-01" ! -newermt "2022-04-30" | wc -l
+--To find the image files for the month of April--
+find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -ls |grep 'Jul'
+--To find the number of images of April month--
+find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -ls |grep 'Jul'|wc -l
+
