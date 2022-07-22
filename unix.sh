4b1b2cb379586925b58298ccb35128a29c174353
Manvi Kapoor <manvi.kapoor@shorthillstech.com>
2022-07-22 16:33:17 +0530

    Apr corrected

--- unix.sh
+++ unix.sh
@@ -3,7 +3,7 @@ find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -ls -newermt "2021-
 --To find the number of images of April month--
 find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -newermt "2022-04-01" ! -newermt "2022-04-30" | wc -l
 --To find the image files for the month of April--
-find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -ls |grep 'Jul'
+find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -ls |grep 'Apr'
 --To find the number of images of April month--
-find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -ls |grep 'Jul'|wc -l
+find . \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -ls |grep 'Apr'|wc -l
 
